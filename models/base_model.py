5a766b495f0e3386062e8bfbd7efc9df6d3416f0
hedi <hedihamida98@gmail.com>
2022-10-11 20:44:45 +0100

    new

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,11 @@ class BaseModel:
     updated_at = datetime.now()
 
 
-    def __init__(self, name):
+    def __init__(self):
         id = str(uuid.uuid4())
         created_at = datetime.now()
         updated_at = datetime.now()
-        
+
     def __str__(self):
         print(f"[{self.name}] ({self.id}) {self.__dict__}")
     def save(self):
