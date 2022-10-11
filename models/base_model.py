8af694760f5464a57d4650d7bfa3e18227f61d59
hedi <hedihamida98@gmail.com>
2022-10-11 21:27:23 +0100

    new

--- models/base_model.py
+++ models/base_model.py
@@ -5,11 +5,8 @@ class BaseModel:
     """BaseModel Class"""
     
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4)
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        
         if kwargs:
-             
             for key, value in kwargs.items():
                 if key == "id":
                     self.id = str(value)
@@ -20,6 +17,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4)
             self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         var = (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
