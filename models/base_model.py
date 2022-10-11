c3f3c0a6c6c459fb0872f00f6c2781be6711372d
hedi <hedihamida98@gmail.com>
2022-10-11 21:19:08 +0100

    new

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,8 @@ class BaseModel:
     """BaseModel Class"""
     
     def __init__(self, *args, **kwargs):
-        
+        self.id = str(uuid.uuid4)
+        self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if (kwargs):
             if kwargs: 
