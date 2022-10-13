f77efcde8c827ec326d647b963ab586598f55778
hedi <hedihamida98@gmail.com>
2022-10-13 02:06:18 +0100

    new

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+uinitest for the amenity
+"""
+
+import unittest
+
+from models.place import Place
+
+""" class Test_amenity"""
+class Test_amenity(unittest.TestCase):
+    """ class Test_amenity"""
+
+     
+    def test(self):
+        self.assertEqual(str, type(Place.name))
+    
+    def test1(self):
+        self.assertEqual(list, type(Place.amenity_ids))
+    
+    def test2(self):
+        self.assertEqual(str, type(Place.city_id))
+    
+    def test3(self):
+        self.assertEqual(float, type(Place.latitude))
+    
+    def test4(self):
+        self.assertEqual(float, type(Place.longitude))
+
+    
+
