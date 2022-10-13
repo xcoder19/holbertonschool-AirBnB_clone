4e0c23d24b34085576e2c8bf7f4eb08dc36bc42c
hedi <hedihamida98@gmail.com>
2022-10-13 08:48:37 +0100

    new

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -19,9 +19,9 @@ class FileStorage:
         self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
-
+        obj = self.__objects
         with open(self.__file_path, "w") as file:
-            json.dump(self.__objects, file)
+            json.dump(obj, file)
 
     def reload(self):
         try:
