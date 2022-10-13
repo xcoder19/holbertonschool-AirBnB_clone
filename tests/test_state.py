775f12453007b639cdba98158fd39dca206f2b3c
hedi <hedihamida98@gmail.com>
2022-10-13 01:45:37 +0100

    new

---
+++ tests/test_state.py
@@ -0,0 +1,11 @@
+import unittest
+
+from models import state
+
+""" class Test_State"""
+class Test_State(unittest.TestCase):
+    """ class Test_State"""
+
+     
+    def test(self):
+        self.assertEqual(str, type(state.name))
