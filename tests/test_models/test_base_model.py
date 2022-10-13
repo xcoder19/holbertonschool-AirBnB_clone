21ba110a2a9cfdcc5947675687cd166137fb49b6
hedi <hedihamida98@gmail.com>
2022-10-13 02:27:33 +0100

    new

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,4 +14,11 @@ class Test_BaseModel(unittest.TestCase):
         obj2 = BaseModel()
         self.assertNotEqual(obj1.id, obj2.id)
     
+    def test2(self):
+        
+        obj1 = BaseModel()
+        obj2 = BaseModel()
+        
+        self.assertLess(obj2.updated_at, obj1.updated_at)
+
     
