cd30739b6c51e5a69e1cd462932af8e0612cf8a9
hedi <hedihamida98@gmail.com>
2022-10-13 02:07:33 +0100

    new

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+uinitest for the city
+"""
+
+import unittest
+
+from models.review import Review
+
+""" class Test_State"""
+class Test_State(unittest.TestCase):
+    """ class Test_State"""
+
+     
+    def test(self):
+        self.assertEqual(str, type(Review.place_id))
+
+    def test2(self):
+        self.assertEqual(str, type(Review.user_id))
